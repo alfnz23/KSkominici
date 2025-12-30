3c64dd5062e0e5fc432d97085efe9e49172d2448
alfnz23 <redlinebadpacks@gmail.com>
2025-12-30 22:14:48 +0100

    Update PassportForm.tsx

--- app/components/PassportForm.tsx
+++ app/components/PassportForm.tsx
@@ -868,6 +868,7 @@ export default function PassportForm() {
                             <option value="technická místnost">technická místnost</option>
                             <option value="koupelna">koupelna</option>
                             <option value="WC">WC</option>
+                            <option value="chodba">chodba</option>
                             <option value="pokoj">pokoj</option>
                           </select>
                         </div>
